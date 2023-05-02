837bfd29fc769bf1de4a887780276de7f6144bd0
Théo Pioto <127546787+Pi0to@users.noreply.github.com>
2023-05-02 20:21:34 -0300

    Update ex_003.c

--- ex_003.c
+++ ex_003.c
@@ -1,32 +1,27 @@
+#include <stdio.h>
+
 int main() {
 
     int dia, mes, ano;
    
-    printf("Digite o dia: ");
-        scanf("%d", &dia);
-   
-    printf("Digite o mes: ");
-        scanf("%d", &mes);
-   
-    printf("Digite o ano: ");
-        scanf("%d", &ano);
+    printf ("Digite a data no formato dd/mm/aaaa: ");
+        scanf ("%d/%d/%d", &dia, &mes, &ano);
        
     if (dia > 31 || dia < 1 || mes > 12 || ano < 1){
-   
+
         printf ("Data inválida!");
-   
     }
-   
+    
     else if (dia == 31 && ( mes == 4 || mes == 6 || mes == 9 || mes == 11) ){
-       
-        printf ("Data inválida!");
-       
+
+        printf ("Data inválida!"); 
     }
-   
+    
     else if (dia == 29 &&  0 != ano%4){
      
         printf ("Data inválida!");
     }    
+    
     else {
         printf ("Data válida!");
     }
