e69373af5eab596c22427f00e38246e1ea54627a
Théo Pioto <127546787+Pi0to@users.noreply.github.com>
2023-05-03 11:02:34 -0300

    ex_004.c

--- ex_004.c
+++ ex_004.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 
 int main() {
-    float temp;
+    
+    float temp, celsius, fahrenheit;
     int opcao;
 
     printf("Digite a temperatura: ");
-    scanf("%f", &temp);
+        scanf("%f", &temp);
 
     printf("Escolha a opcao de conversao: \n");
+    
     printf("1 - Celsius para Fahrenheit \n");
+   
     printf("2 - Fahrenheit para Celsius \n");
-    scanf("%d", &opcao);
+        scanf("%d", &opcao);
 
     if (opcao == 1) {
-        float fahrenheit = (temp * 9 / 5) + 32;
+        
+        fahrenheit = (temp * 9 / 5) + 32;
         printf("%.2f graus Celsius equivalem a %.2f graus Fahrenheit\n", temp, fahrenheit);
-    } else if (opcao == 2) {
-        float celsius = (temp - 32) * 5 / 9;
+    } 
+    else if (opcao == 2) {
+        
+        celsius = (temp - 32) * 5 / 9;
         printf("%.2f graus Fahrenheit equivalem a %.2f graus Celsius\n", temp, celsius);
-    } else {
+    } 
+    else {
+        
         printf("Opcao invalida\n");
     }
 
