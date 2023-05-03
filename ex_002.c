02304bbcbdedd782ae15f2910d3d00b561c7b555
Théo Pioto <127546787+Pi0to@users.noreply.github.com>
2023-05-03 11:51:30 -0300

    correção-da-merda-que-eu-fiz.c

--- ex_002.c
+++ ex_002.c
@@ -6,11 +6,12 @@ int main() {
     float lado_1, lado_2, lado_3;
     float p, area;
 
-    printf("Digite os valores dos lados do triangulo: ");
-    scanf("%f %f %f", &lado_1, &lado_2, &lado_3);
+    printf("Digite os valores dos lados do triangulo separados por um espaço: ");
+        scanf("%f %f %f", &lado_1, &lado_2, &lado_3);
 
     // Verifica se os lados formam um triângulo válido
-    if (lado_1 + lado_2 <= lado_3 || lado_1 + lado_3 <= lado_2 || lado_2 + lado_3 <= lado_1) {
+    if (lado_1 + lado_2 <= lado_3 || lado_1 + lado_3 <= lado_2 || lado_2 + lado_3 <= lado_1) 
+    {
         printf("Valores invalidos.\n");
     } 
     else {
