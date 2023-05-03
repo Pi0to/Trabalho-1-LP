da195dd472530baef68feeb1a58ce571938867d0
Théo Pioto <127546787+Pi0to@users.noreply.github.com>
2023-05-03 11:29:18 -0300

    ex_005_bonito.c

--- ex_005.c
+++ ex_005.c
@@ -3,19 +3,19 @@
 int main() {
     int num, centena, dezena, unidade;
 
-    printf("Digite um número inteiro de três dígitos: ");
-    scanf("%d", &num);
+    printf ("Digite um número inteiro de três dígitos: ");
+        scanf ("%d", &num);
 
     if (num < 100 || num > 999) {
+        
         printf("Erro: número invalido!\n");
-        return 0;
     }
 
     centena = num / 100;
     dezena = (num / 10) % 10;
     unidade = num % 10;
 
-    printf("O número invertido eh: %d%d%d\n", unidade, dezena, centena);
+    printf ("O número invertido eh: %d%d%d\n", unidade, dezena, centena);
 
     return 0;
 }
